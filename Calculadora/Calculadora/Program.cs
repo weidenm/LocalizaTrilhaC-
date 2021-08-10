51d4a449ec67b5b294f4365ad5b59865287de57b
Weiden <weidencore@gmail.com>
2021-08-09 21:59:53 -0300

    Usando XUnit - Testes de unidade

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class Calculo
+    {
+        public static double Somar(double numero1, double numero2)
+        {
+            return (numero1 + numero2);
+        }
+
+        public static double Subtrair(double numero1, double numero2)
+        {
+            return (numero1 - numero2);
+        }
+
+        public static double Multiplicar(double numero1, double numero2)
+        {
+            return (numero1 * numero2);
+        }
+
+        public static double Dividir(double numero1, double numero2)
+        {
+            return (numero1 / numero2);
+        }
+
+    }
+}
---
+++ Calculadora/CalculosTest/CalculoTeste.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculosTest
+{
+    public class CalculoTeste
+    {
+        [Fact]
+        public void Somar_DoisDouble_RetornarDouble()
+        {
+            var num1 = 10.4;
+            var num2 = 5.6;
+            var valorEsperado = 16;
+
+            var soma = Calculadora.Calculo.Somar(num1, num2);
+            Assert.Equal(valorEsperado, soma);
+
+        }
+
+        [Fact]
+        public void Subtrair_DoisDouble_RetornarDouble()
+        {
+            var num1 = 10.4;
+            var num2 = 5;
+            var valorEsperado = 5.4;
+
+            var subtrair = Calculadora.Calculo.Subtrair(num1, num2);
+            Assert.Equal(valorEsperado, subtrair);
+
+        }
+    }
+}
